e3968eba9f1bd0a3cfe1360697ca728fc260b056
Leandro Bouza <averno.leo@gmail.com>
2022-10-15 19:52:35 -0300

    Se crean index_dos, index_tres

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from ejemplo.models import Familiar
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "ejemplo/saludar.html",{"nombre":"Leandro"},{"apellido":"Bouza"})
+
+
+def index_dos(request,nombre,apellido):
+    return render(request, "ejemplo/saludar.html",{"nombre":nombre},{"apellido":apellido})
+
+def index_tres(request):
+    return render(request,"ejemplo/saludar.html",{"notas":[1,2,3,4,5,6,7,8,9,10]})
 
 
 def monstrar_familiares(request):
--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ejemplo.views import index, monstrar_familiares
+from ejemplo.views import index, index_dos, index_tres, monstrar_familiares
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludar/',index),
+    path('saludar/<nombre>/<apellido>/',index_dos),
+    path('mostrar-notas/',index_tres),
     path('mi-familia/', monstrar_familiares),
 ]
